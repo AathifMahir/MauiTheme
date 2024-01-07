749e88c6dbf5f6b1f309b80f5df718c563822317
Aathif_Mahir <aathifmahir@gmail.com>
2024-01-07 22:57:32 +0530

    Span Extensions

---
+++ src/MauiTheme/MauiTheme/Extensions/SpanExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiTheme.Extensions;
+internal static class SpanExtension
+{
+    internal static bool CompareSpanChar(this ReadOnlySpan<char> value, ReadOnlySpan<char> other)
+    {
+        return value.CompareTo(other, StringComparison.OrdinalIgnoreCase) == 0;
+    }
+}
