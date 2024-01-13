88937f00114eed64d2b8f8f7800638157534d77f
Aathif_Mahir <aathifmahir@gmail.com>
2024-01-14 00:01:27 +0530

    New Theme Core

---
+++ src/MauiTheme.Core/IMauiThemeHybrid.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace MauiTheme.Core;
+public interface IMauiThemeHybrid
+{
+    public MauiAppTheme CurrentAppTheme { get; set; }
+    public string CurrentResource { get; set; }
+}
---
+++ src/MauiTheme.Core/ThemeConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiTheme.Core;
+public sealed class ThemeConfiguration
+{
+    public MauiAppTheme DefaultTheme { get; set; } = MauiAppTheme.Unspecified;
+    public string[] DefaultStyleResources { get; set; } = Array.Empty<string>();
+    public Dictionary<string, string> Resources { get; set; } = new();
+}
