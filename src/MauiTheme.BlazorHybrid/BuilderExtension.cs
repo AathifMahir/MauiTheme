11b727362c4410e00ef614c8e40dddf7cd13a0a2
Aathif_Mahir <aathifmahir@gmail.com>
2024-01-15 03:24:29 +0530

    Improved Hybrid Implementations

--- src/MauiTheme.BlazorHybrid/BuilderExtension.cs
+++ src/MauiTheme.BlazorHybrid/BuilderExtension.cs
@@ -4,13 +4,34 @@ using Microsoft.Extensions.DependencyInjection;
 namespace MauiTheme.BlazorHybrid;
 public static class BuilderExtension
 {
-    public static IServiceCollection UseMauiThemeHybrid(this IServiceCollection services, IMauiTheme clientTheme, DependencyType dependencyType = DependencyType.Scoped)
+    /// <summary>
+    /// Initializes the Maui Theme for a Blazor Hybrid Project. Ensure not to use this BuilderExtension on Classic Blazor Projects.
+    /// </summary>
+    /// <param name="clientTheme">An instance of IMauiThemeHybrid that needs to be provided from the Blazor Hybrid Project.</param>
+    /// <param name="dependencyType">Indicates whether the dependency should be stored as Scoped or Singleton. Defaults to Scoped.</param>
+    public static IServiceCollection UseMauiThemeHybrid(this IServiceCollection services, IMauiThemeHybrid clientTheme, DependencyType dependencyType = DependencyType.Scoped)
     {
         if (dependencyType == DependencyType.Scoped)
-            services.AddScoped<IMauiThemeHybrid>((_) => new MauiThemeHybridDefault(clientTheme));
+            services.AddScoped((_) => clientTheme);
         else
-            services.AddSingleton<IMauiThemeHybrid>(new MauiThemeHybridDefault(clientTheme));
+            services.AddSingleton(clientTheme);
 
         return services;
     }
+
+    /// <summary>
+    /// Initializes the Maui Theme for a Classic Blazor Project. Ensure not to use this BuilderExtension on Blazor Hybrid Projects.
+    /// </summary>
+    /// <param name="suppressException">Indicates whether to suppress exceptions. Defaults to True.</param>
+    /// <param name="dependencyType">Indicates whether the dependency should be stored as Scoped or Singleton. Defaults to Scoped.</param>
+    public static IServiceCollection UseMauiThemeBlazor(this IServiceCollection services, bool suppressException = true, DependencyType dependencyType = DependencyType.Scoped)
+    {
+        if (dependencyType == DependencyType.Scoped)
+            services.AddScoped<IMauiThemeHybrid>((_) => new MauiThemeHybridDefault(suppressException));
+        else
+            services.AddSingleton<IMauiThemeHybrid>(new MauiThemeHybridDefault(suppressException));
+
+        return services;
+    }
+
 }
--- src/MauiTheme.BlazorHybrid/MauiThemeHybridDefault.cs
+++ src/MauiTheme.BlazorHybrid/MauiThemeHybridDefault.cs
@@ -1,22 +1,44 @@
 ﻿using MauiTheme.Core;
+using MauiTheme.Core.Exceptions;
 
 namespace MauiTheme.BlazorHybrid;
 internal sealed class MauiThemeHybridDefault : IMauiThemeHybrid
 {
+    readonly MauiAppTheme _currentAppTheme = MauiAppTheme.Unspecified;
     public MauiAppTheme CurrentAppTheme 
     {
-        get => _clientTheme.CurrentAppTheme;
-        set => _clientTheme.CurrentAppTheme = value;
-    }
-    public string CurrentResource 
-    {
-        get => _clientTheme.CurrentResource;
-        set => _clientTheme.CurrentResource = value;
+        get => _currentAppTheme;
+        set
+        {
+            if(value == _currentAppTheme) return;
+            if (_suppressException) 
+            {
+                Console.WriteLine("Theme Change is Not Supported in Blazor, Only Supported on Blazor Hybrid Which Runs on Native Invocation");
+                return;
+            }
+            throw new MauiThemeException("Theme Change is Not Supported in Blazor, Only Supported on Blazor Hybrid Which Runs on Native Invocation");
+        }
     }
 
-    private readonly IMauiTheme _clientTheme;
-    public MauiThemeHybridDefault(IMauiTheme clientTheme)
+    readonly string _currentRersource = string.Empty;
+    public string CurrentResource 
     {
-        _clientTheme = clientTheme;
+        get => _currentRersource;
+        set
+        {
+            if(value == _currentRersource) return;
+            if (_suppressException) 
+            {
+                Console.WriteLine("Resource Change is Not Supported in Blazor, Only Supported on Blazor Hybrid Which Runs on Native Invocation");
+                return;
+            }
+            throw new MauiThemeException("Resource Change is Not Supported in Blazor, Only Supported on Blazor Hybrid Which Runs on Native Invocation");
+        }
+    }
+
+    readonly bool _suppressException;
+    public MauiThemeHybridDefault(bool suppressException)
+    {
+        _suppressException = suppressException;
     }
 }
