aefd9b45a141e8b041741fe4c5919c54fedda5f3
Aathif_Mahir <aathifmahir@gmail.com>
2024-01-19 09:17:26 +0530

    Core Improvements

---
+++ src/MauiTheme.Core/Events/ThemeModeChangedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTheme.Core.Events;
+public sealed class ThemeModeChangedEventArgs : EventArgs
+{
+    public ThemeMode CurrentTheme { get; set; }
+
+    public ThemeModeChangedEventArgs(ThemeMode currentTheme)
+    {
+        CurrentTheme = currentTheme;
+    }
+}
